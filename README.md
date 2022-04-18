ff0ecb7199cb25781767e28967b052e58672a3f7
TymurL <logvinenko814@gmail.com>
2022-04-18 22:50:43 +0300

    Complete task 1

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 Написати код продюсера, що імітує потік даних, який генерується в певній предметній області
 (наприклад, показники масиву сенсорів, дані про угоди купівлі/продажу валюти або цінних паперів, логи тощо),
 та надсилає її до певного топіку Кафка.
+
+Steps to show work:
+1. Start ZooKeeper `bin/zookeeper-server-start.sh config/zookeeper.properties`
+2. Start Kafka `bin/kafka-server-start.sh config/server.properties`
+3. Create topic `bin/kafka-topics.sh --create --topic my-topic --bootstrap-server localhost:9092`
+4. Read events `bin/kafka-console-consumer.sh --topic my-topic --from-beginning --bootstrap-server localhost:9092`
+5. Run producer (this also needs to be done in wsl due to some problem with interaction with broker in wsl)
+6. Stop Kafka broker
+7. Stop ZooKeeper
+8. Delete data of local env `rm -rf /tmp/kafka-logs /tmp/zookeeper`
--- src/main/java/ua/kpi/tymur/Main.java
+++ src/main/java/ua/kpi/tymur/Main.java
@@ -1,7 +1,39 @@
 package ua.kpi.tymur;
 
+import com.github.javafaker.Faker;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hellooo!!!");
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 10000; i++) {
+            var producerRecord = new ProducerRecord<>(
+                    "my-topic",
+                    String.valueOf(i),
+                    String.format("Pokemon %s from %s has been found!",
+                            faker.pokemon().name(), faker.pokemon().location())
+            );
+            producer.send(producerRecord);
+
+            // For first lab delay can be 1 second.
+            Thread.sleep(1000);
+        }
+
+        producer.close();
     }
 }
